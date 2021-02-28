adb66898b373238d1556660064f3af99fe832bf2
SCREENSABER <61020218+SCREENSABER@users.noreply.github.com>
2021-02-28 09:04:23 -0600

    pushing file

---
+++ app.js
@@ -0,0 +1,9 @@
+ console.log("Let's get ready to party with jQuery!");
+$('article').addClass("text-center");
+$("article p.last-child").remove();
+$('title').css("font-size", Math.random(1 * 100))
+$('<li>Get groceries</li>').appendTo('ul')
+$('li').text('')
+$('img').on('click', function(){
+    this.remove
+})
